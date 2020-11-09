d01b8b9b7fb8f6eb7b2463f28d88b227a6401afb
João Lassi <jmlassi@outlook.com>
2020-11-09 18:28:36 -0300

    Ainda alterando para testes

--- src/main/java/com/eva/resource/TokenResource.java
+++ src/main/java/com/eva/resource/TokenResource.java
@@ -17,7 +17,7 @@ public class TokenResource {
 	public void revoke(HttpServletRequest req, HttpServletResponse resp) {
 		Cookie cookie = new Cookie("refreshToken", null);
 		cookie.setHttpOnly(true);
-		cookie.setSecure(false); // TODO: Em producao sera true
+		cookie.setSecure(true); // TODO: Em producao sera true
 		cookie.setPath(req.getContextPath() + "/oauth/token");
 		cookie.setMaxAge(0);
 		
