f5d5d2684c139eb2f59dc24d0fd9d00921c0369f
João Lassi <jmlassi@outlook.com>
2020-11-09 19:00:07 -0300

    Ainda alterando para testes

--- src/main/java/com/eva/EvartsApplication.java
+++ src/main/java/com/eva/EvartsApplication.java
@@ -2,8 +2,12 @@ package com.eva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.eva.config.property.EvaApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(EvaApiProperty.class)
 public class EvartsApplication {
 
 	public static void main(String[] args) {
