43c3b006df4d974628a5743e72cbf30e9cdf8a85
João Lassi <jmlassi@outlook.com>
2020-11-10 11:31:27 -0300

    Ainda alterando para testes

--- src/main/java/com/eva/cors/CorsFilter.java
+++ src/main/java/com/eva/cors/CorsFilter.java
@@ -36,7 +36,7 @@ public class CorsFilter implements Filter {
 		
 		response.setHeader("Access-Control-Allow-Origin", evaApiPorperty.getOriginPermitida());
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Set-Cookie", "SameSite=None; Secure");
+        response.setHeader("Set-Cookie", "SameSite=Strict;");
 		
 		if ("OPTIONS".equals(request.getMethod()) && evaApiPorperty.getOriginPermitida().equals(request.getHeader("Origin"))) {
 			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
