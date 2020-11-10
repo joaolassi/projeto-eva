b6ed76ebd1194760a49dba08af6e18187c3101ea
João Lassi <jmlassi@outlook.com>
2020-11-10 17:30:00 -0300

    Ainda alterando para testes

---
+++ src/main/java/com/eva/config/SameSiteFilter.java
@@ -0,0 +1,20 @@
+package com.eva.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+public class SameSiteFilter extends GenericFilterBean {
+    @Override
+    public void doFilter (ServletRequest request,  ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setHeader("Set-Cookie", "HttpOnly; SameSite=strict");
+        chain.doFilter(request, response);
+    }
+}
