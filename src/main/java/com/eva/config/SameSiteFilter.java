993988a3386c891b49538490810873fa4440cf6e
João Lassi <jmlassi@outlook.com>
2020-11-10 19:26:07 -0300

    Ainda alterando para testes

--- src/main/java/com/eva/config/SameSiteFilter.java
+++ src/main/java/com/eva/config/SameSiteFilter.java
@@ -14,7 +14,7 @@ public class SameSiteFilter extends GenericFilterBean {
     @Override
     public void doFilter (ServletRequest request,  ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse resp = (HttpServletResponse) response;
-        resp.setHeader("Set-Cookie", "SameSite=None; Secure;");
+        resp.setHeader("Set-Cookie", "SameSite=Strict; Secure;");
         chain.doFilter(request, response);
     }
 }
